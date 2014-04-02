68ab321bb4863990a389ac86d6427f6585a827bb
Calvin Huang <clhuang@eneron.us>
2014-04-02 15:19:32 -0700

    Use SymLogNorm for regular plots; update symmetrical colormap

--- RenderGUI.py
+++ RenderGUI.py
@@ -20,9 +20,40 @@ kivy.require('1.8.0')
 Tk().withdraw()
 
 
+def cmap_map(function, cmap):
+    """ Applies function (which should operate on vectors of shape 3:
+    [r, g, b], on colormap cmap. This routine will break any discontinuous     points in a colormap.
+    """
+    cdict = cmap._segmentdata
+    step_dict = {}
+    # Firt get the list of points where the segments start or end
+    for key in ('red', 'green', 'blue'):
+        step_dict[key] = map(lambda x: x[0], cdict[key])
+    step_list = sum(step_dict.values(), [])
+    step_list = np.array(list(set(step_list)))
+    # Then compute the LUT, and apply the function to the LUT
+    reduced_cmap = lambda step: np.array(cmap(step)[0:3])
+    old_LUT = np.array(map(reduced_cmap, step_list))
+    new_LUT = np.array(map(function, old_LUT))
+    # Now try to make a minimal segment definition of the new LUT
+    cdict = {}
+    for i, key in enumerate(('red', 'green', 'blue')):
+        this_cdict = {}
+        for j, step in enumerate(step_list):
+            if step in step_dict[key]:
+                this_cdict[step] = new_LUT[j, i]
+            elif new_LUT[j, i] != old_LUT[j, i]:
+                this_cdict[step] = new_LUT[j, i]
+        colorvector = map(lambda x: x + (x[1], ), this_cdict.items())
+        colorvector.sort()
+        cdict[key] = colorvector
+
+    return colors.LinearSegmentedColormap('colormap', cdict, 1024)
+
 BUF_DIMENSIONS = (3840, 2160)  # supports up to 4k screens
 LOGNORM = colors.LogNorm()
 SCALAR_MAP = cm.ScalarMappable(LOGNORM, cm.bone)
+SYM_MAP = cmap_map(lambda x: 1 - x, cm.RdBu)
 
 
 class Mode(object):
@@ -288,12 +319,13 @@ class RenderGUI(Widget):
         data = self.raw_data
         bounds = (np.nanmin(data), np.nanmax(data))
         if bounds[0] >= 0:  # use log-based approach
-            SCALAR_MAP.set_norm(LOGNORM)
+            #SCALAR_MAP.set_norm(LOGNORM)
+            SCALAR_MAP.set_norm(colors.SymLogNorm(bounds[1] * 0.1 ** self.log_offset))
             SCALAR_MAP.set_cmap(cm.bone)
-            SCALAR_MAP.set_clim(bounds[1] * 0.1 ** self.log_offset, bounds[1])
+            SCALAR_MAP.set_clim(0, bounds[1])
         else:  # use symlog approach
             b2 = max((abs(bounds[0]), bounds[1]))
-            SCALAR_MAP.set_cmap(cm.PiYG)
+            SCALAR_MAP.set_cmap(SYM_MAP)
             SCALAR_MAP.set_norm(colors.SymLogNorm(b2 * 0.1 ** self.log_offset))
             SCALAR_MAP.set_clim(-b2, b2)
 
